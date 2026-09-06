3d5bb1f2247ad3534d81248cdef6d88b5a8bce96
pratik1216-pixel <pratikkumbhar108@gmail.com>
2026-09-06 22:55:39 +0530

    Add power function for square and cube calculations

---
+++ Practical-5/program.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int power(int n) {
+    return n * n;       
+}
+int power(int n, int x) {
+    return n * n * n;     
+}
+int main() {
+    int n;
+    cout << "Enter number: ";
+    cin >> n;
+    cout << "Square = " << power(n) << endl;
+    cout << "Cube = " << power(n, 3) << endl;
+    return 0;
+}
